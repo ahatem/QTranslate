5c1dac2604140256dccdc72322d48948aea6f963
Ahmed Hatem <ahatem925@gmail.com>
2025-10-06 01:09:53 +0300

    chore(build): include new modules in settings.gradle.kts
    - add "plugins:common" and "plugins:google-services" to the list of included modules
    - this ensures that these modules are recognized and can be built by Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,13 @@ dependencyResolutionManagement {
     }
 }
 
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
+include("api")
 include("core")
+include("plugins:common")
+include("plugins:google-services")
 
 rootProject.name = "qtranslate-app"
-
-include("api")
