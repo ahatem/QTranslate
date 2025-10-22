78ebbaf082cb088eeed8048b53637170460c1afb
Ahmed Hatem <ahatem925@gmail.com>
2025-10-22 23:06:32 +0300

    feat(settings): introduce explicit close dialog event
    - add CloseSettingsDialog event to SettingsEvent
    - improve documentation for ShowMessage event

--- core/src/main/kotlin/com/github/ahatem/qtranslate/core/settings/mvi/SettingsEvent.kt
+++ core/src/main/kotlin/com/github/ahatem/qtranslate/core/settings/mvi/SettingsEvent.kt
@@ -3,7 +3,28 @@ package com.github.ahatem.qtranslate.core.settings.mvi
 import com.github.ahatem.qtranslate.api.plugin.NotificationType
 import com.github.ahatem.qtranslate.core.shared.arch.UiEvent
 
+/**
+ * Events emitted by SettingsStore to the UI layer.
+ *
+ * Events represent one-time occurrences that the UI should react to,
+ * such as showing messages or closing dialogs.
+ */
 sealed interface SettingsEvent : UiEvent {
+
+    /**
+     * Request to close the settings dialog.
+     * Typically emitted after successful save or cancel.
+     */
     data object CloseSettingsDialog : SettingsEvent
-    data class ShowMessage(val message: String, val type: NotificationType) : SettingsEvent
+
+    /**
+     * Request to show a message to the user.
+     *
+     * @property message The message text to display
+     * @property type The notification type (SUCCESS, ERROR, WARNING, INFO)
+     */
+    data class ShowMessage(
+        val message: String,
+        val type: NotificationType
+    ) : SettingsEvent
 }
