a1d05d31e3695bf611b8d7ba5e3495102bd072ac
Ahmed Hatem <ahatem925@gmail.com>
2026-03-21 21:43:39 +0200

    build(project): update module inclusions and structure
    - reorganize module definitions in settings.gradle.kts
    - add ui-swing and app modules to the project build structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,11 +9,17 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
+rootProject.name = "qtranslate-app"
+
 include("api")
+
 include("core")
+include("ui-swing")
+
+include("app")
+
 include("plugins:common")
 include("plugins:google-services")
 include("plugins:bing-services")
 
-rootProject.name = "qtranslate-app"
 
